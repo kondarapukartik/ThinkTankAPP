e4a389b57ad392171a903783ebbd68479d5cf31f
Your Name <you@example.com>
2024-08-19 13:52:24 +0530

    Initial commit

---
+++ thinktank/lib/pages/dailyTarget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DailytargetPage extends StatelessWidget {
+  const DailytargetPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Daily Targets")),
+    );
+  }
+}
